2d8331deccc3d2712c921ba1fe77c1d6ba9d7795
GHNetCode <125825587+GHNetCode@users.noreply.github.com>
2024-06-06 17:38:29 +0100

    Add files via upload

--- app/script.js
+++ app/script.js
@@ -29,7 +29,9 @@ function circularText(txt1, radius, classIndex) {
   
   
 
-//pointerdown includes both click and touch events..
+//pointerdown includes both pointerdown and touchscreen events
+//Must be used within an if('onpointerdown'..){}else{if('onclick'..) }
+//to work correctly for both events on the same "click"..
 let lstMnuEvt= null; // last Menu events...
 if ('onpointerdown' in window) {
     document.addEventListener("pointerdown", (event) => { //For touch screens only.. 
@@ -42,30 +44,47 @@ if ('onpointerdown' in window) {
         console.log('all  elementCls :'+ elementCls[0])//We assuming obj has only 1 element!
 
 
-        //if open, close menu by clicking\touching other parts of the screen
+        //if open, close menu by clicking\touching other parts of the screen..
          //elementId :menu__toggle - menu has been opened\closed..
          //elementCls :menu__item  - Link in menu has been pressed.
          //elementCls :menu__box   - pressed inside menu box
          //elementCls :ctr0        - pressed on the background
         if (elementId==="menu__toggle"){// Let`s track it with lstEvntClId
-            console.log("menu now toggled..")
+            //console.log("menu now toggled..")
         }
-
         if (lstMnuEvt==="menu__toggle"&&elementCls[0]==="menu__box"||
             elementCls[0]==="ctr0"||elementCls[0]==="ctr1"
-            
         ){
            //  console.log("menu Was toggled, and no menu items were pushed, close the menu..") 
              document.getElementById("menu__toggle").checked = false; 
-            
              console.log("Need to ensure all menu items are set!!")
         }
-        lstMnuEvt = elementId; //
+        lstMnuEvt = elementId;//set lstMnuEvt at the End of menu if statement(s)..
+     })}else{if ('onclick' in window){
+//      window.preventDefault();
+      document.addEventListener("click", (event) => { // For mouse clicks only..
+          
+          let elementId = event.target.id;
+          let elementCls = event.target.classList; //event.target.classList.contains("my-class")
+         console.log('click elementId : '+ elementId);
+         console.log('click elementCls : '+ elementCls);
+      
+         if (elementId==="menu__toggle"){// Let`s track it with lstEvntClId
+          console.log("menu now toggled..")
+          //document.getElementById("menu__toggle").checked = true;
+        //  menu__toggle_check();
+      }
 
-
-
-
-     })}
+      if (lstMnuEvt==="menu__toggle"&&elementCls[0]!=="menu__item"
+      ){
+         //  console.log("menu Was toggled, and no menu items were pushed, close the menu..") 
+           document.getElementById("menu__toggle").checked = false; 
+          // menu__toggle_uncheck();
+           console.log("Need to ensure all menu items are set!!")
+      }
+      lstMnuEvt = elementId;//set lstMnuEvt at the End of menu if statement(s)..
+      
+      }    ) }}
 
 
 /*
