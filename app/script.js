764315244312758bd5d5a5f010e514ec1cab8a36
GHNetCode <125825587+GHNetCode@users.noreply.github.com>
2024-06-07 14:28:56 +0100

    Add files via upload

--- app/script.js
+++ app/script.js
@@ -4,13 +4,11 @@ window.onresize=()=>{
     }
 
 
-//shapes button..
+//floating shapes button..--------------------------------
     let animatemeid_Btn = document.getElementById('animatemeid_Btn');//shapesBtn for floating shapes..
     let animatemeid = document.getElementById('animatemeid');//shapesBtn for floating shapes..
- 
-    // On pointerdown..
-    animatemeid_Btn.addEventListener("pointerdown",e =>{ //("pointerdown",e =>{
-        console.log("e: "+e.pointerId)
+     animatemeid_Btn.addEventListener("pointerdown",e =>{ //("pointerdown",e =>{
+    //  console.log("e: "+e.pointerId)
         animatemeid_Btn.classList.toggle("animatemeCl_BtnOn");
         animatemeid.classList.toggle("animatemeCL_On");
 })
@@ -25,17 +23,14 @@ function circularText(txt1, radius, classIndex) {
       origin += deg;});
   }
   circularText("shapes", 24, 0);
- 
-  
-  
+ //floating shapes button..--------------------------------
 
-//pointerdown includes both pointerdown and touchscreen events
-//Must be used within an if('onpointerdown'..){}else{if('onclick'..) }
-//to work correctly for both events on the same "click"..
+
+//Handel pointer screen events (includes touch\click events to a degree..)
 let lstMnuEvt= null; // last Menu events...
-if ('onpointerdown' in window) {
+if ('onpointerdown' in window){
     document.addEventListener("pointerdown", (event) => { //For touch screens only.. 
-
+        
         //get id and class events from '(event)' for Variables above..
         let elementId = event.target.id;
         let elementCls = event.target.classList; //event.target.classList.contains("my-class")
@@ -43,36 +38,33 @@ if ('onpointerdown' in window) {
         console.log('all  elementId :'+ elementId);
         console.log('all  elementCls :'+ elementCls[0])//We assuming obj has only 1 element!
 
-
         //if menu open, close by clicking\touching other parts of the screen..
-        if (elementId==="menu__toggle"){// Let`s track it with lstEvntClId
-            //console.log("menu now toggled..")
-        }
-        if (lstMnuEvt==="menu__toggle"&&elementCls[0]==="menu__box"||
-            elementCls[0]==="ctr0"||elementCls[0]==="ctr1"||elementCls[0]==="H2_pb"){
-      
-           //  console.log("menu Was toggled, and no menu items were pushed, close the menu..") H2_pb
- 
-             document.getElementById("menu__toggle").checked = false;  
+            if (document.getElementById("menu__toggle").checked===true){
+                if (elementCls[0]==="menu__box"||elementCls[0]==="ctr0"||elementCls[0]==="ctr1"||elementCls[0]==="H2_pb")
+                    { document.getElementById("menu__toggle").checked = false;  
+                    console.log("Need to ensure all menu items are set!!") }          
+              }
+        
+})}
 
-             console.log("Need to ensure all menu items are set!!")
+//on scrolling fade containers in and out..-------------------------------
+function scrlfInfOutFunc() {
+    let fInfOut = document.querySelectorAll(".scrlfInfOutCl");
+  
+    for (let i = 0; i < fInfOut.length; i++) {
+        let windowHeight = window.innerHeight;
+        let elementTop = fInfOut[i].getBoundingClientRect().top;
+        let elementVisible = 10;
+  
+      if (elementTop < windowHeight - elementVisible) {
+            fInfOut[i].classList.add("active");
+          } else {
+            fInfOut[i].classList.remove("active");
         }
-        lstMnuEvt = elementId;//set lstMnuEvt at the End of menu if statement(s)..
-        })}
-       //else{if ('onclick' in window){
-//     //   window.preventDefault();
-       // document.addEventListener("click", (event) => { // For mouse clicks only..
-       //     let elementId = event.target.id;
-       //     let elementCls = event.target.classList; //event.target.classList.contains("my-class")
-       //    console.log('click elementId : '+ elementId);
-       //    console.log('click elementCls : '+ elementCls);
-       //if (lstMnuEvt==="menu__toggle"&&elementCls[0]!=="menu__item"
-       // ){//console.log("menu Was toggled, and no menu items were pushed, close the menu..") 
-       //      document.getElementById("menu__toggle").checked = false; 
-       //      console.log("Need to ensure all menu items are set!!")
-       // }
-       // lstMnuEvt = elementId;//set lstMnuEvt at the End of menu if statement(s)..
-       // })}}
+}}
+  window.addEventListener("scroll", scrlfInfOutFunc);
+  //on scrolling fade containers in and out..-------------------------------
+
 
 
 /*
@@ -85,21 +77,6 @@ Undefined
 Number
 Null
 
-
-
-
-
-
-
-
-Boolean
-Object
-Symbol
-String
-Bigint
-undefined
-NUll
-Number
 
 
 
